4f182628b44c912fa61d9de62eb60b0cc371793e
Combiz0 <firdaus@combiz.org>
2024-09-02 16:05:34 +0530

    Update urls.py

--- Excel_Project/Excel_Project/urls.py
+++ Excel_Project/Excel_Project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Excel_app.urls')),
+    path('api/',include('Excel_app.urls')),
     
 ]
 
