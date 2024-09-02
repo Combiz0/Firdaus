a98b1de0ce7b8fc39ff21c1f71df99b1059c7644
Combiz0 <firdaus@combiz.org>
2024-09-02 16:05:04 +0530

    Update urls.py

--- Excel_Project/Excel_app/urls.py
+++ Excel_Project/Excel_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 # from .views import CheckFileView
 urlpatterns = [
-    path('',views.add_email,name='add_email'),
+    path('add/',views.add_email,name='add_email'),
     path('delete/<str:email>/', views.delete_email, name='delete_email'),
     path('check/<str:email>/', views.check_email, name='check_email'),
     # path('get-expiry/<str:email>/', views.check_email, name='manage-expiry'),/
